e4dcc0c1d351e82174e400e522a180fdb9cb1622
Ankush Anshuman <ankush3107@gmail.com>
2018-06-16 20:34:56 +0530

    Initial commit

---
+++ app/src/main/java/com/example/mahe/sos/MainActivity.java
@@ -0,0 +1,224 @@
+package com.example.mahe.sos;
+
+import android.Manifest;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationManager;
+import android.provider.Settings;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GoogleApiAvailability;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import java.text.DateFormat;
+
+public class MainActivity extends AppCompatActivity implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener, LocationListener {
+
+    private static final int MY_PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION = 1;
+    private static final String ANONYMOUS = "anonymous";
+    GoogleApiClient gac;
+    LocationRequest locationRequest;
+    private static final int UPDATE_INTERVAL = 15 * 1000;
+    private static final int FASTEST_UPDATE_INTERVAL = 2 * 1000;
+    FusedLocationProviderClient mFusedLocationProviderClient;
+    Location lastLocation;
+    TextView tv;
+    private FirebaseAuth mFirebaseAuth;
+    private FirebaseUser mFirebaseuser;
+    private String mUsername;
+    private String mPhotoUrl;
+
+    private static final String TAG = "MainActivity";
+
+    @Override
+    protected void onStart() {
+        gac.connect();
+        super.onStart();
+    }
+
+    @Override
+    protected void onStop() {
+        gac.disconnect();
+        super.onStop();
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        tv=findViewById(R.id.textView);
+        mUsername = ANONYMOUS;
+        mFirebaseAuth=FirebaseAuth.getInstance();
+        mFirebaseuser=mFirebaseAuth.getCurrentUser();
+        if(mFirebaseuser==null)
+        {
+            startActivity(new Intent(this,LoginActivity.class));
+            finish();
+            return;
+        }
+        else {
+            mUsername=mFirebaseuser.getDisplayName();
+            if(mFirebaseuser.getPhotoUrl()!=null)
+                mPhotoUrl=mFirebaseuser.getPhotoUrl().toString();
+        }
+
+
+
+        locationRequest = LocationRequest.create()
+                .setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY)
+                .setInterval(UPDATE_INTERVAL)
+                .setFastestInterval(FASTEST_UPDATE_INTERVAL);
+
+
+
+        LocationManager service = (LocationManager) getSystemService(LOCATION_SERVICE);
+        boolean enabled = service.isProviderEnabled(LocationManager.GPS_PROVIDER);
+        if (!enabled) {
+            Intent intent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+            startActivity(intent);
+        }
+
+        gac = new GoogleApiClient.Builder(this)
+                .addApi(LocationServices.API)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .build();
+
+    }
+
+
+    public void startSos(View view) {
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+//        mFusedLocationProviderClient.getLastLocation()
+//                .addOnCompleteListener(this, new OnCompleteListener<Location>() {
+//                    @Override
+//                    public void onComplete(@NonNull Task<Location> task) {
+//                        if (task.isSuccessful() && task.getResult() != null) {
+//                            lastLocation = task.getResult();
+//
+////                            txtLatitude.setText(String.valueOf(lastLocation.getLatitude()));
+////                            txtLongitude.setText(String.valueOf(lastLocation.getLongitude()));
+//
+//                        } else {
+//                            Log.w(TAG, "getLastLocation:exception", task.getException());
+////                            showSnackbar(getString(R.string.no_location_detected));
+//                        }
+//                    }
+//                });
+
+        LocationManager lm= (LocationManager) getSystemService(LOCATION_SERVICE);
+        Toast.makeText(this, "GPS location without google client\n"+lm.getLastKnownLocation(LocationManager.GPS_PROVIDER).toString(), Toast.LENGTH_SHORT).show();
+
+
+
+    }
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED
+                && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(MainActivity.this,
+                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                    MY_PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION);
+
+            return;
+        }
+        else{
+//            Toast.makeText(this, "Permission is given", Toast.LENGTH_SHORT).show();
+            Log.d(TAG, "Permission is already given");
+        }
+
+        /* mFusedLocationProviderClient= LocationServices.getFusedLocationProviderClient(this);
+        mFusedLocationProviderClient.getLastLocation()
+                .addOnSuccessListener(this, new OnSuccessListener<Location>() {
+                    @Override
+                    public void onSuccess(Location location) {
+                        // Got last known location. In some rare situations, this can be null.
+
+                        if (location != null) {
+                            // Logic to handle location object
+                            lastLocation=location;
+                        }
+                    }
+                });
+        */
+        Location ll = LocationServices.FusedLocationApi.getLastLocation(gac);
+        // TODO: use fusedlocaionproviderclient
+
+        Log.d(TAG, "LastLocation from Deprecated: " + (ll == null ? "NO LastLocation" : ll.toString()));
+        tv.setText("LastLocation from Deprecated: " + (ll == null ? "NO LastLocation" : ll.toString()));
+//        Log.d(TAG, "LastLocation: " + (ll == null ? "NO LastLocation" : lastLocation.toString()));
+
+        LocationServices.FusedLocationApi.requestLocationUpdates(gac, locationRequest, this);
+
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+        Toast.makeText(MainActivity.this, "onConnectionFailed: \n" + connectionResult.toString(),
+                Toast.LENGTH_LONG).show();
+        Log.d(TAG, connectionResult.toString());
+
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        if (location != null) {
+            updateUI(location);
+        }
+
+    }
+    private void updateUI(Location loc) {
+        Log.d(TAG, "updateUI");
+        tv.setText(Double.toString(loc.getLatitude()) + '\n' + Double.toString(loc.getLongitude()) + '\n' + DateFormat.getTimeInstance().format(loc.getTime()));
+    }
+    private boolean isLocationEnabled() {
+        LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        return locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER) ||
+                locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER);
+    }
+
+    public void firebaseSignOut(View view) {
+        mFirebaseAuth.signOut();
+        startActivity(new Intent(MainActivity.this,LoginActivity.class));
+        finish();
+    }
+}
